9088a58020a6696fa652f99af47843a4558139f7
rohitdhingra <er.rohit.dhingra@gmail.com>
2025-03-22 22:07:29 +0530

    basic unit test cases and multiple build tool flavours

--- CalculatePlainIDE/src/com/learning/Calculator.java
+++ CalculatePlainIDE/src/com/learning/Calculator.java
@@ -4,7 +4,4 @@ public class Calculator {
     public int integerDivision(int dividend, int divisor) {
         return dividend / divisor;
     }
-    public int integerSubtraction(int minuend, int subtrahend) {
-        return minuend - subtrahend;
-    }
 }
---
+++ CalculatorGradleProject/src/test/java/learning/CalculatorTest.java
@@ -0,0 +1,20 @@
+package learning;
+
+import com.learning.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+
+    //Arrange
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void testIntegerDivision_whenValidValuesProvided_shouldReturnExpectedResult() {
+        //Act
+        int result = calculator.integerDivision(8, 2);
+        //Assert
+        assertEquals(4, result,"The result of 8 divided by 2 should be 4");
+    }
+}
