5e7535f69070c6ba2f3b7126d3d70ce665f1d3c8
rohitdhingra <er.rohit.dhingra@gmail.com>
2025-03-22 13:55:03 +0530

    restructuring test case

--- calculator/src/test/java/com/learning/CalculatorTest.java
+++ calculator/src/test/java/com/learning/CalculatorTest.java
@@ -2,17 +2,18 @@ package com.learning;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CalculatorTest {
 
+    //Arrange
     private final Calculator calculator = new Calculator();
 
     @Test
     void testIntegerDivision_whenValidValuesProvided_shouldReturnExpectedResult() {
+        //Act
         int result = calculator.integerDivision(8, 2);
-        if (result == 4) {
-            System.out.println("Test passed");
-        } else {
-            System.out.println("Test failed");
-        }
+        //Assert
+        assertEquals(4, result,"The result of 8 divided by 2 should be 4");
     }
 }
