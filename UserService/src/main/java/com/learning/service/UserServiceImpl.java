53179dc593baf4548373452cf447561d2a40604b
rohitdhingra <er.rohit.dhingra@gmail.com>
2025-03-23 17:39:11 +0530

    tdd approach

--- UserService/src/main/java/com/learning/service/UserServiceImpl.java
+++ UserService/src/main/java/com/learning/service/UserServiceImpl.java
@@ -2,10 +2,14 @@ package com.learning.service;
 
 import com.learning.entities.User;
 
+import java.util.UUID;
+
 public class UserServiceImpl implements UserService {
     @Override
     public User createUser(String firstName, String lastName, String email, String password, String repeatPassword) {
 
-        return new User();
+        if(firstName.isEmpty())
+            throw new IllegalArgumentException("User FirstName is Mandatory");
+        return new User(firstName,lastName,email, UUID.randomUUID().toString());
     }
 }
--- UserService/src/test/java/com/learning/service/UserServiceTest.java
+++ UserService/src/test/java/com/learning/service/UserServiceTest.java
@@ -1,20 +1,33 @@
 package com.learning.service;
 
 import com.learning.entities.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+    String repeatPassword;
+    UserService userService;
+
+    @BeforeEach
+    void init()
+    {
+        firstName = "Rohit";
+        lastName = "Sharma";
+        email = "rsharma@gmail.com";
+        password = "12345678";
+        repeatPassword = "12345678";
+        userService = new UserServiceImpl();
+    }
     @Test
     void testCreateUser_whenUserDetailsProvided_returnsUserObject(){
         //Arrange
-        String firstName = "Rohit";
-        String lastName = "Sharma";
-        String email = "rsharma@gmail.com";
-        String password = "12345678";
-        String repeatPassword = "12345678";
-        UserService userService = new UserServiceImpl();
+
 
 
         //Act
@@ -23,5 +36,26 @@ class UserServiceTest {
         //Assert
 
         assertNotNull(user,"create method should not be null");
+        assertEquals(firstName,user.getFirstName(),"First name should be same as provided");
+        assertEquals(lastName,user.getLastName(),"Last name should be same as provided");
+        assertEquals(email,user.getEmail(),"Email should be same as provided");
+        assertNotNull(user.getId());
     }
+
+    @Test
+    void testCreateUser_whenFirstNameIsEmpty_throwsIllegalArgumentException()
+    {
+        //Arrange
+       firstName = null ;
+
+
+
+        //Act & Assert
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> userService.createUser(firstName, lastName, email, password, repeatPassword));
+
+        //Assert
+        assertEquals("User FirstName is Mandatory",thrown.getMessage());
+
+    }
+
 }
