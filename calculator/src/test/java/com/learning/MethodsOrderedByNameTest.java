46bda13b25e7eed361f7934723ae85da3f70562f
rohitdhingra <er.rohit.dhingra@gmail.com>
2025-03-23 14:01:55 +0530

    adv ordering test examples

--- calculator/src/test/java/com/learning/MethodsOrderedByNameTest.java
+++ calculator/src/test/java/com/learning/MethodsOrderedByNameTest.java
@@ -1,30 +1,40 @@
 package com.learning;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @TestMethodOrder(MethodOrderer.MethodName.class)
 class MethodsOrderedByNameTest {
+
+    StringBuilder completed = new StringBuilder("");
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("The state of each method"+completed);
+    }
     @Test
     void testD()
     {
         System.out.println("Executing testD");
+        completed.append("1");
     }
     @Test
     void testA()
     {
         System.out.println("Executing testA");
+        completed.append("2");
     }
     @Test
     void testC()
     {
         System.out.println("Executing testC");
+        completed.append("3");
     }
     @Test
     void testB()
     {
         System.out.println("Executing testB");
+        completed.append("4");
     }
 
 
